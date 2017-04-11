2dfc440dcb1d7baa8ed5839cbd4a47b7fa682802
Matt Wisch <matt.wisch@rackspace.com>
2017-04-11 14:24:46 -0400

    Added dialog to specify the search basedomain
    You can now search for different base domains, or use the default of rackspace to see what kind of cookies you have lingering.

--- rackspace_cookies.py
+++ rackspace_cookies.py
@@ -11,6 +11,10 @@ basepath = "{}/.mozilla/firefox/*.default/cookies.sqlite" .format(os.environ['HO
 sqldb = glob.glob(basepath)[0]
 MAXDATE = 2049840000
 
+basedom = raw_input("Enter the baseDomain you would like to search for (Default is: rackspace): ")
+if not basedom:
+    basedom = "rackspace"
+
 if not os.path.isfile(sqldb):
     print "Could not find cookies DB at: {}" .format(sqldb)
 
@@ -30,7 +34,7 @@ while True:
         conn = sqlite3.connect(sqldb)
         cur = conn.cursor()
         try:
-            data = cur.execute("select * from moz_cookies where baseDomain like '%rackspace%'")
+            data = cur.execute("select * from moz_cookies where baseDomain like '%{}%'" .format(basedom))
         except sqlite3.Error, e:
             print 'Error {0}: ' .format(e.args[0])
             sys.exit(1)
@@ -43,13 +47,13 @@ while True:
             table.add_row([cookie[0], cookie[3], created, expires, cookie[5], cookie[6], cookie[4]])
 
         print table
-        del_id = raw_input("\nIf you would like to delete a cookie, enter the ID, to quit hit Enter: ")
-        if del_id:
+        del_id = raw_input("\nTo delete a cookie, enter the ID. To exit, enter 'quit'. To refresh, hit Enter. INPUT: ")
+        if del_id == "quit":
+            conn.close()
+            sys.exit(0)
+        elif del_id:
             int(del_id)
             cur.execute("delete from moz_cookies where id='{}'" .format(del_id))
             conn.commit()
-        else:
-            conn.close()
-            sys.exit(0)
     except ValueError:
         print "The ID must be numerical!"
